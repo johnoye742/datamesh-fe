b764b13e7a47338d8163857922ea2b1117625fd9
John Oye <66364739+johnoye742@users.noreply.github.com>
2025-12-06 04:29:16 +0000

    Added Toast shadcn/ui component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata, Viewport } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import { Toaster } from "@/components/ui/sonner"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -23,7 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+        <main>
+          {children}
+        </main>
+        <Toaster />
+      </body>
     </html>
   )
 }
